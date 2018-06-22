354f1ce1e6f8cec7b73e3c61674b5202106f6284
rozaydin <ridvan.ozaydin@siemens.com>
2018-06-22 18:14:01 +0300

    processing expressions in value annotations

--- src/main/java/com/rhtech/newstack/config/ServerConfig.java
+++ src/main/java/com/rhtech/newstack/config/ServerConfig.java
@@ -2,26 +2,37 @@ package com.rhtech.newstack.config;
 
 import io.undertow.Undertow;
 import io.undertow.server.RoutingHandler;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 public class ServerConfig {
 
-  @Autowired Environment env;
-
   @Bean(name = "undertow", destroyMethod = "stop")
-  public Undertow createServer(RoutingHandler handler) {
-    return Undertow.builder().addHttpListener(8080, "localhost").setHandler(handler).build();
+  public Undertow createServer(
+      RoutingHandler handler,
+      @Value("${server.hostname}") String hostname,
+      @Value("${server.port}") int port) {
+    return Undertow.builder().addHttpListener(port, hostname).setHandler(handler).build();
   }
 
   @Bean(name = "routingHandler")
   public RoutingHandler createRoutingHandler() {
     RoutingHandler routingHandler = new RoutingHandler();
     return routingHandler;
+  }
+
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer createPropertySourcesPlaceholderConfigurer(
+      Environment env) {
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer =
+        new PropertySourcesPlaceholderConfigurer();
+    propertySourcesPlaceholderConfigurer.setEnvironment(env);
+    return propertySourcesPlaceholderConfigurer;
   }
 }
